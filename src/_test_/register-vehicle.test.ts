64d29991b35cceed4a9d868476568e13efafe2b8
unknown <javalencia790@gmail.com>
2021-08-18 21:16:19 -0500

    refactor: refactor Register vehicle

--- src/_test_/register-vehicle.test.ts
+++ src/_test_/register-vehicle.test.ts
@@ -1,4 +1,4 @@
-import { RegisterVehicle  } from '../controllers/register-vehicle' 
+import { RegisterVehicle } from '../controllers/register-vehicle'
 
 describe('RegisterVehicle ', () => {
   test('is the name not exist return 400 ', () => {
@@ -7,24 +7,57 @@ describe('RegisterVehicle ', () => {
       body: {
         // name: 'Nisan',
         model: 'Dtx',
-        year: 2020
+        year: 2020,
+        color: 'color'
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Error in the: name'))
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Error in the: name'))
   })
+
   test('is the model not exist return 400 ', () => {
     const sut = new RegisterVehicle()
     const httpRequest = {
       body: {
         name: 'Nisan',
         // model: 'Dtx',
-        year: 2020
+        year: 2020,
+        color: 'color'
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Error in the: model'))
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Error in the: model'))
+  })
+
+  test('is the year not exist return 400 ', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Nisan',
+        model: 'Dtx',
+        color: 'color'
+        // year: 2020
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Error in the: year'))
+  })
+
+  test('is the color not exist return 400 ', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Nisan',
+        model: 'Dtx',
+        year: 2020
+        // color: 'color'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Error in the: color'))
   })
 })
--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -3,16 +3,14 @@ import { HttpRequest, HttpResponse } from '../interfaces/http-interface'
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export class RegisterVehicle {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Error in the: name')
-      }
-    }
-    if (!httpRequest.body.model) {
-      return {
-        statusCode: 400,
-        body: new Error('Error in the: model')
+    const requiredProperties = ['name', 'model', 'year', 'color']
+
+    for (const props of requiredProperties) {
+      if (!httpRequest.body[props]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Error in the: ${props}`)
+        }
       }
     }
   }
